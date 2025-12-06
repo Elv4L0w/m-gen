c8e2eabdef197ff27e507e88d67cd0021aa83e6f
Elv4L0w <91699930+Elv4L0w@users.noreply.github.com>
2025-12-06 22:21:39 +0100

    another change to fix docker

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="127.0.0.1", port=port, debug=True)
-    # 0.0.0.0 za lan
+    app.run(host="0.0.0.0", port=port, debug=True)
+    # 0.0.0.0 za lan more ko je docker v vprasanju
